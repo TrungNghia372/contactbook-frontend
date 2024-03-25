dd91cc8493a465f07255357ba137402277720500
TrungNghia372 <nghiab2005725@student.ctu.edu.vn>
2024-03-26 00:45:32 +0700

    Tao trang them moi mot lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="page">
+        <h4>Thêm Liên Hệ</h4>
+        <ContactForm
+            :contact="contact"
+            @submit:contact="submitContact"
+        />
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+    import ContactForm from "../components/ContactForm.vue";
+    import ContactService from "../services/contact.service"
+    export default {
+        components: {
+            ContactForm,
+        },
+        data() {
+            return {
+                contact: {},
+                message: "",
+            };
+        },
+        methods: {
+            async submitContact(data) {
+                try {
+                    await ContactService.create(data);
+                    this.message = "Liên hệ được cập nhật thành công.";
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        },
+        created() {
+            this.message = "";
+        },
+    };
+</script>
